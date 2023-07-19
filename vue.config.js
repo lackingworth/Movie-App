d680e88646f6ca7c1b847368b116d5a8a69538d2
lackingworth <worstface@yandex.ru>
2023-07-19 19:54:31 +0300

    Added vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+
   publicPath: process.env.NODE_ENV === 'production'
     ? '/Movie-App/'
-    : '/'
+    : '/',
+
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
+  }
 })
